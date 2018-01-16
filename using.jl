62feadfbeabbde895eb2261b4a787b4198084a30
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-01-16 17:24:28 -0500

    update

--- using.jl
+++ using.jl
@@ -11,13 +11,17 @@ loggerCRlin = get_logger("CRlin")
 loggerCGlin = get_logger("CGlin")
 loggerArm = get_logger("Armijo")
 loggerLin = get_logger("Linesearch")
+loggerlaunch = get_logger("launch_linesearch.jl")
 
 loggerCRlin.level = MiniLogging.ERROR
 loggerCGlin.level = MiniLogging.ERROR
 loggerArm.level = MiniLogging.ERROR
 loggerLin.level = MiniLogging.ERROR
+# loggerlaunch.level = MiniLogging.ERROR
 
 include("CGlin.jl")
 include("CRlin.jl")
 include("Armijo.jl")
 include("Linesearch.jl")
+include("LCG.jl")
+include("LCR.jl")
